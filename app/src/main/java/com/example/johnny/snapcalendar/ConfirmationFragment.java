8db17cb2f529e5f05141c0aa51440a959da52600
bkzhang <brian.k.zhang@gmail.com>
2017-01-22 11:23:14 -0500

    google1daymaybe

---
+++ app/src/main/java/com/example/johnny/snapcalendar/ConfirmationFragment.java
@@ -0,0 +1,89 @@
+package com.example.johnny.snapcalendar;
+
+import android.app.DialogFragment;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+
+public class ConfirmationFragment extends DialogFragment {
+    String date;
+    Bitmap poster;
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageReference = storage.getReferenceFromUrl("gs://snapcalender.appspot.com");
+    StorageReference posterStorageReference = storageReference.child("Poster");
+
+    public Bitmap getPoster(Bitmap bitmap) {
+        poster = bitmap;
+        return poster;
+    }
+    public String getDate (String textblock){
+        date = textblock;
+        return date;
+    }
+    public void upload (Bitmap bitmap){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = posterStorageReference.putBytes(data);
+        StorageMetadata metadata = new StorageMetadata.Builder()
+                .setContentType("image/jpg")
+                .build();
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                dismiss();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Uri downloadUrl = taskSnapshot.getMetadata().getDownloadUrl();
+            }
+        });
+        dismiss();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_confirmation, container, false);
+        getDialog().setTitle("Simple Dialog");
+        Button dismiss = (Button) rootView.findViewById(R.id.dismiss);
+        Button interested = (Button) rootView.findViewById(R.id.interested);
+        ImageView preview = (ImageView) rootView.findViewById(R.id.posterView);
+        TextView displayDate = (TextView) rootView.findViewById(R.id.dateView);
+
+        displayDate.setText(getDate(date));
+        preview.setImageBitmap(poster);
+
+        dismiss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        interested.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                upload(poster);
+            }
+        });
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/johnny/snapcalendar/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.johnny.snapcalendar;
+
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> output = new ArrayList<String>();
+    TextView text;
+    private String title;
+    ImageView mimageView;
+    Bitmap mphoto;
+    TextRecognizer textRecognizer;
+    StringBuilder t= new StringBuilder();;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
+        mimageView = (ImageView) this.findViewById(R.id.image_from_camera);
+        textRecognizer = new TextRecognizer.Builder(this).build();
+        Intent intent = getIntent();
+
+        if(intent.getParcelableExtra(Cam.image)!=null){
+
+            Bitmap bitmap = (Bitmap) intent.getParcelableExtra(Cam.image);
+            mimageView.setImageBitmap(bitmap);
+            try {
+                SparseArray<TextBlock> foo = getText(bitmap);
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            FragmentManager fm = getFragmentManager();
+            ConfirmationFragment dialogFragment = new ConfirmationFragment ();
+           // dialogFragment.getDate(foo.get(0).getValue());
+            dialogFragment.getPoster(bitmap);
+            dialogFragment.show(fm, "Sample Fragment");
+        }
+
+    }
+
+    public void takeImageFromCamera(View view){
+        Intent intent = new Intent(this, Cam.class);
+        startActivity(intent);
+    }
+
+
+    public SparseArray<TextBlock> getText(Bitmap bitmap) throws ParseException, IOException {
+        Frame outputFrame = new Frame.Builder().setBitmap(bitmap).build();
+        SparseArray<TextBlock> foo = findText(outputFrame);
+        if(foo.get(0)!=null) {
+            for(int i =0;i<foo.size();i++) {
+                output.add(i, foo.get(i).getValue());
+                t.append(foo.get(i)+"\n");
+
+                //Log.d("MAIN", output.get(i));
+
+
+                try{
+                    DateParse date = new DateParse();
+                    date.printDate(output.get(i));
+                    Log.d("worked", "worked");
+                } catch(ParseException e) {
+                    Log.d("not worked", "unworked");
+                }
+            }
+            String bar=t.toString();
+            text.setText(bar);
+            title = output.get(0);
+            GoogleCalendar calendar = new GoogleCalendar();
+//            GoogleCalendar calendar = new GoogleCalendar()
+//            DateTime startDateTime = calendar.startDateTime(output[0]);
+//            EventDateTime start = calendar.startEventDateTime(startDateTime, "America/New_York");
+//            event.setStart(start);
+//            EventDateTime end = calendar.endEventDateTime(startDateTime, "America/New_York");
+//            event.setEnd(end);
+//            String[] recurrence = new String[] {"RRULE:FREQ=DAILY;COUNT=2"};
+//            event.setRecurrence(Arrays.asList(recurrence));
+//            EventReminder[] reminderOverrides = new EventReminder[] {
+//                    new EventReminder().setMethod("email").setMinutes(24 * 60),
+//                    new EventReminder().setMethod("popup").setMinutes(10),
+//            };
+//            Event.Reminders reminders = new Event.Reminders()
+//                    .setUseDefault(false)
+//                    .setOverrides(Arrays.asList(reminderOverrides));
+//            event.setReminders(reminders);
+
+//            String calendarId = "primary";
+//            event = calendar.getService().events().insert(calendarId, event).execute();
+//            System.out.printf("Event created: %s\n", event.getHtmlLink());
+
+
+        }
+        else{
+            Log.d("MAIN", "NOOOOOOOOOOOOOOOO TEXTTTTTTTT");
+        }
+        return foo;
+    }
+
+    private SparseArray<TextBlock> findText(Frame frame) {
+        SparseArray<TextBlock> foo=textRecognizer.detect(frame);
+        return foo;
+    }
+}
